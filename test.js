eaaf3a22a72c513cc05f6c7589ac6572aea81066
Kale <sung-ho@naver.com>
2018-06-28 18:00:29 +0900

    test case

---
+++ test.js
@@ -0,0 +1,13 @@
+const ipInfo = require('./index.js');
+
+console.log(ipInfo('14.1.128.2'));
+console.log(ipInfo('19.128.4.2'));
+console.log(ipInfo('91.3.4.2'));
+console.log(ipInfo('75.3.5.2'));
+console.log(ipInfo('14.25.2.2'));
+console.log(ipInfo('52.4.0.2'));
+console.log(ipInfo('42.128.3.2'));
+console.log(ipInfo('123.1.126.2'));
+console.log(ipInfo('1.128.3.2'));
+console.log(ipInfo('25.4.128.2'));
+console.log(ipInfo('55.128.5.2'));
